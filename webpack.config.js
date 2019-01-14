22b22a54d7cd8e691c7209fdc2ed30e168435270
Surendhar95 <Surendhar95@users.noreply.github.com>
2019-01-14 21:15:18 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,99 @@
-let path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-let nodeExternals = require('webpack-node-externals');
-const moduleObj = {
-    rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: ["babel-loader"],
-        }
-    ],
-};
-const client = {
-    entry: {
-    'client': './src/client/index.js'
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack")
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html",
+});
+module.exports = {
+  entry: [
+     'babel-polyfill',
+     './src'
+  ],
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
   },
-  target: 'web',
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist/public')
+    publicPath: "/"
   },
-  module: moduleObj,
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    contentBase: './src'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          query: {
+                    presets: ['es2015', 'stage-2', 'react']
+                 }
+        }
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: "react-svg-loader",
+            options: {
+              jsx: true // true outputs JSX tags
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+                context: '../assets',
+                name: 'root[path][name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes')
+              ],
+            },
+          },
+        ],
+      },
+    ]
+  },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: 'src/client/index.html'
-    })
+    htmlWebpackPlugin,
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify("staging") })
   ]
 };
-const server = {
-    entry: {
-        'server': './src/server/index.js'
-    },
-    target: 'node',
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: moduleObj,
-    externals: [nodeExternals()]
-}
-module.exports = [client, server];
